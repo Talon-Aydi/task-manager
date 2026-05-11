5bf8f7645419c35f8561a6ba0e5eb91cf16956b1
Talon <ak1998@live.nl>
2026-05-12 00:01:28 +0200

    Pest test have been added to creating a task

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable(['id', 'naam'])]
 class Category extends Model
 {
     public function task()
