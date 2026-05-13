a682b389f8a3dc8cd2347f67fa56d2569c3a8497
Talon <ak1998@live.nl>
2026-05-13 15:21:00 +0200

    Removed redundant variable

--- app/View/Components/Tasks/Card.php
+++ app/View/Components/Tasks/Card.php
@@ -10,19 +10,11 @@ use App\Enums\TaskStatus;
 
 class Card extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public $statusColor; 
-
     public function __construct(public Task $task)
     {
         $this->task = $task;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.tasks.card');
